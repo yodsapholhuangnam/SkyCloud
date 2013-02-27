3fb6e095e0b3bee4934a5ba9fff0788a6ecae424
Yodsaphol Huangnam <jefasa.shining@gmail.com>
2013-02-27 21:56:45 +0100

    Create a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SkyCloud::Application.config.secret_token = 'c0bb4246237783a7e751aca446d7132112351c9fe25d53953f8fc7ae0378d00537c408e99d0362c21f13fa15bb22347a75bd14443b6292af63136b768d1c7643'
